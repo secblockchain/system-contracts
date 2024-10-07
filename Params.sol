4b98893e1c3aefcb46c2f05a3aa39378034225c1
secblockchain <blockchain@smart-energy-token.com>
2024-10-07 09:53:03 +0200

    Added system contracts

---
+++ Params.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Params {
+    bool public initialized;
+
+    // System contracts
+    address
+        public constant ValidatorContractAddr = 0x000000000000000000000000000000000000f000;
+    address
+        public constant PunishContractAddr = 0x000000000000000000000000000000000000F001;
+    address
+        public constant ProposalAddr = 0x000000000000000000000000000000000000F002;
+
+    // System params
+    uint16 public MaxValidators = 21;
+    // Validator have to wait StakingLockPeriod blocks to withdraw staking
+    uint64 public constant StakingLockPeriod = 86400;
+    // Validator have to wait WithdrawProfitPeriod blocks to withdraw his profits
+   // uint64 public constant WithdrawProfitPeriod = 28800;
+    uint256 public MinimalStakingCoin = 32 ether;
+    // minimum initial staking to become a validator
+    uint256 public minimumValidatorStaking = 1000000 ether;
+
+
+    // percent distrubution of Gas Fee earned by validator 100000 = 100%
+    uint public stakerPartPercent = 45000;              //45%
+    uint public validatorPartPercent = 30000;           //30%
+    uint public burnPartPercent = 25000;                //25%
+    uint public contractPartPercent = 0;                //0%
+    uint public burnStopAmount = 100000000000 ether;    // after 100 billion coins burn, it will stop burning
+    uint public totalBurnt;
+
+
+
+    modifier onlyMiner() {
+        require(msg.sender == block.coinbase, "Miner only");
+        _;
+    }
+
+    modifier onlyNotInitialized() {
+        require(!initialized, "Already initialized");
+        _;
+    }
+
+    modifier onlyInitialized() {
+        require(initialized, "Not init yet");
+        _;
+    }
+
+    modifier onlyPunishContract() {
+        require(msg.sender == PunishContractAddr, "Punish contract only");
+        _;
+    }
+
+    modifier onlyBlockEpoch(uint256 epoch) {
+        require(block.number % epoch == 0, "Block epoch only");
+        _;
+    }
+
+    modifier onlyValidatorsContract() {
+        require(
+            msg.sender == ValidatorContractAddr,
+            "Validators contract only"
+        );
+        _;
+    }
+
+    modifier onlyProposalContract() {
+        require(msg.sender == ProposalAddr, "Proposal contract only");
+        _;
+    }
+}
